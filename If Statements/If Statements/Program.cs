5dcdc711ff2f3385c2d0cad8476f7a1abace1e72
Muluken Mekuriya <mulerthegrete@gmail.com>
2022-04-12 12:12:51 -0400

    if statment with bool

---
+++ If Statements/If Statements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = false;
+
+            if (isMale)
+            {
+                Console.WriteLine("You are male");
+            }
+            else { Console.WriteLine("You are not male"); }
+
+            Console.ReadLine();
+        }
+    }
+}

e10012a06ef80b54657502985e0d71ceadfb09af
Muluken Mekuriya <mulerthegrete@gmail.com>
2022-04-12 11:46:50 -0400

    Cube method created, that return cube power of the argument

---
+++ Return Statement/Return Statement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Return_Statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cube(2);
+            Console.WriteLine(Cube(2));
+
+        }
+        static int Cube(int num)
+        {
+            int result = num*num*num;
+            return result;
+        }
+    }
+}

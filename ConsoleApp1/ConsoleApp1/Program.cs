1ad61927db7bc46e29574ab016e9df95b1064b74
Muluken Mekuriya <mulerthegrete@gmail.com>
2022-04-14 13:57:16 -0400

    Swapping user inpute data

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the First Number: ");
+            string inpute1 = Console.ReadLine();
+            Console.WriteLine("Input the Second Number: ");
+            string inpute2 = Console.ReadLine();
+            Console.WriteLine("Expected Output:");
+            Console.WriteLine("After Swapping :");
+            Console.WriteLine($"First Number :{inpute2}");
+            Console.WriteLine($"Second Number :{inpute1}");
+
+        }
+    }
+}
---
+++ Number Swapping/Number Swapping/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Swapping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Write a C# Sharp program to swap two numbers.
+            Test Data:
+            Input the First Number : 5
+            Input the Second Number : 6
+            Expected Output:
+            After Swapping :
+            First Number : 61
+            Second Number : 5
+                        */
+            Console.WriteLine("Input the First Number: ");
+            string inpute1 = Console.ReadLine();
+            Console.WriteLine("Input the Second Number: ");
+            string inpute2 = Console.ReadLine();
+            Console.WriteLine("Expected Output:");
+            Console.WriteLine("After Swapping :");
+            Console.WriteLine($"First Number :{inpute2}");
+            Console.WriteLine($"Second Number :{inpute1}");
+        }
+    }
+}

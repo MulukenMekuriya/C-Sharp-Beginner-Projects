3d5e5ba58e858b7b47597bd304a88ff3cc1eba13
Muluken Mekuriya <mulerthegrete@gmail.com>
2022-04-19 13:39:04 -0400

    math operation with user inpute completed

---
+++ Average of user inpute data/Average of user inpute data/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Average_of_user_inpute_data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Write a C# Sharp program that takes four numbers as input to calculate and print the average
+             */
+            int inpute1, inpute2, inpute3, inpute4;
+            Console.WriteLine("Enter inpute 1");
+                inpute1 =int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter inpute 2");
+            inpute2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter inpute 3");
+            inpute3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter inpute 4");
+            inpute4 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Expeceted output is : {(inpute1+inpute2+inpute3+inpute4)/4}");
+
+
+        }
+    }
+}
---
+++ Maths operation with user inpute/Maths operation with user inpute/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maths_operation_with_user_inpute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Write a C# Sharp program to print on screen the output of adding, subtracting, multiplying and dividing of two numbers which will be entered by the user. Go to the editor
+Test Data:
+             */
+            int inpute1, inpute2;
+
+            Console.WriteLine("Enter the first number: ");
+            inpute1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            inpute2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Expected output:");
+            Console.WriteLine($"inpute1+inpute2 = {inpute1 + inpute2}");
+            Console.WriteLine($"inpute1-inpute2 = {inpute1 - inpute2}");
+            Console.WriteLine($"inpute1*inpute2 = {inpute1 * inpute2}");
+            Console.WriteLine($"inpute1/inpute2 = {inpute1 / inpute2}");
+            Console.WriteLine($"inpute1%inpute2 = {inpute1 % inpute2}");
+
+        }
+    }
+}
